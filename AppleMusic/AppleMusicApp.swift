e51e97ccfed2ae2d7a5c86550adcbcb9b0e44556
lmrYoke <luke@yokeglobal.com>
2021-08-19 18:38:56 -0700

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Luke Martin-Resnick on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
